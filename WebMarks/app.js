0ebaa1e87d2e5b4c03a10e482e44fda0a001755c
veggiesaurus <accomrie@gmail.com>
2014-02-04 16:17:27 +0200

    error handling

--- WebMarks/app.js
+++ WebMarks/app.js
@@ -75,42 +75,66 @@ app.get("/:id([0-9]+):courseType(w|f|h|s)\/:studentID", function(req, res)
     //get course info
     connection.query("SELECT * FROM info WHERE `coursePrefix` = '"+urlCourseCode+"'",function(err, rows, fields)
     {
-        if (err) throw err;
+        if (err)
+        {
+            res.render("errorDatabase", {errorInfo:"Error getting course info",urlCourseCode:urlCourseCode});
+            return;
+        }
         if (rows && rows.length)
         {
             //get the student info
             connection.query("SELECT * FROM `"+urlCourseCode+"_students` WHERE studentID = '"+studentID+"'",function(errStudent, rowsStudent, fieldsStudent)
             {
-                if (errStudent) throw errStudent;
+                if (errStudent)
+                {
+                    res.render("errorDatabase", {errorInfo:"Error getting student info",urlCourseCode:urlCourseCode,studentID:studentID});
+                    return;
+                }
                 if (rowsStudent && rowsStudent.length)
                 {
                     //get the categories
                     connection.query("SELECT * FROM `"+urlCourseCode+"_categories`",function(errCats, rowsCats, fieldsCats)
                     {                 
-                        if (errCats) throw errCats;
+                        if (errCats)
+                        {
+                            res.render("errorDatabase", {errorInfo:"Error getting categories",urlCourseCode:urlCourseCode,studentID:studentID});
+                            return;
+                        }
                         if (rowsCats && rowsCats.length)
                         {   
                             //get all the marks                   
                             connection.query("SELECT mark,maxMarks,entryName,typeID FROM ?? tabMarks, ?? tabEntries WHERE (studentID=? AND tabMarks.entryID = tabEntries.entryID) ORDER BY tabMarks.entryID",[urlCourseCode+"_marks",urlCourseCode+"_entries",studentID],function(errMarks, rowsMarks, fieldsMarks)
                             {                 
-                                if (errMarks) throw errMarks;
+                                if (errMarks)
+                                {
+                                    res.render("errorDatabase", {errorInfo:"Error getting marks",urlCourseCode:urlCourseCode,studentID:studentID});
+                                    return;
+                                }
                                 if (rowsMarks && rowsMarks.length)
                                 {
 
                                     //get averages
                                     connection.query("SELECT CAST(average as SIGNED) as average,typeID FROM ?? tabAverages WHERE (studentID=?) ORDER BY typeID",[urlCourseCode+"_averages",studentID],function(errAverages, rowsAverages, fieldsAverages)
                                     {
-                                        if (errAverages) throw errAverages;
+                                        if (errAverages)
+                                        {
+                                            res.render("errorDatabase", {errorInfo:"Error getting averages",urlCourseCode:urlCourseCode,studentID:studentID});
+                                            return;
+                                        }
                                         if (rowsAverages && rowsAverages.length)
                                         {
                                             //get histograms
                                             connection.query("SELECT tabCategories.categoryName, tabHistograms.*  FROM ?? tabHistograms, ?? tabCategories WHERE (tabHistograms.typeID = tabCategories.typeID AND tabCategories.displayHistogram = True)",[urlCourseCode+"_histograms",urlCourseCode+"_categories"],function(errHists, rowsHists, fieldsHists)
                                             {
-                                                if (errHists) throw errHists;
+                                                if (errHists)
+                                                {
+                                                    res.render("errorDatabase", {errorInfo:"Error getting histograms",urlCourseCode:urlCourseCode,studentID:studentID});
+                                                    return;
+                                                }
                                                 if (rowsHists && rowsHists.length)
-                                                    res.render("index", {courseCode:urlCourseCode, courseName:rows[0].courseName, numCategories:rows[0].numCategories, updateDate:rows[0].updateDate.toDateString(), cats:rowsCats, averages:rowsAverages,studentID:rowsStudent[0].studentID, studentName:rowsStudent[0].name, marks:rowsMarks, hists:JSON.stringify(rowsHists)});
+                                                    res.render("marks", {courseCode:urlCourseCode, courseName:rows[0].courseName, numCategories:rows[0].numCategories, updateDate:rows[0].updateDate.toDateString(), cats:rowsCats, averages:rowsAverages,studentID:rowsStudent[0].studentID, studentName:rowsStudent[0].name, marks:rowsMarks, hists:JSON.stringify(rowsHists)});
                                                 else
-                                                    res.render("index", {courseCode:urlCourseCode, courseName:rows[0].courseName, numCategories:rows[0].numCategories, updateDate:rows[0].updateDate.toDateString(), cats:rowsCats, averages:rowsAverages,studentID:rowsStudent[0].studentID, studentName:rowsStudent[0].name, marks:rowsMarks, hists:-1});
+                                                    res.render("marks", {courseCode:urlCourseCode, courseName:rows[0].courseName, numCategories:rows[0].numCategories, updateDate:rows[0].updateDate.toDateString(), cats:rowsCats, averages:rowsAverages,studentID:rowsStudent[0].studentID, studentName:rowsStudent[0].name, marks:rowsMarks, hists:-1});
                                             
                                             });
                                         }
@@ -136,11 +160,26 @@ app.get("/:id([0-9]+):courseType(w|f|h|s)\/:studentID", function(req, res)
 
 app.get("/:id([0-9]+):courseType(w|f|h|s)\?", function(req, res)
 {
-    res.render("noStudent");
+    res.render("selectStudent");
 });
 
-app.get("/", function(req, res){	
-	res.render("noCourse");
+app.get("/", function(req, res)
+{	
+    var urlCourseCode=req.params.id+req.params.courseType;
+    var studentID=req.params.studentID;
+    //get course info
+    connection.query("SELECT coursePrefix, courseName FROM `info`",function(err, rows, fields)
+    {
+        if (err) throw err;
+        if (rows && rows.length)
+        {
+            res.render("selectCourse", {courseInfo:rows});
+        }
+        else
+        {
+            res.render("errorDatabase");
+        }
+    });
 });
 
 app.listen(port);
